268deb7b0dbcd22f153877d8e7d3e2371c2c93db
cliffeby <clifford.eby@gmail.com>
2018-04-19 10:58:35 -0400

    Added crop stats on images at framNo

--- credentials.py
+++
@@ -1,4 +0,0 @@
-login = {
-    "ConnectionString" : "HostName=Duckpin.azure-devices.net;DeviceId=Lane4;SharedAccessKey=8Y0kDpjDbfFBlCVRonFqKRHzSoY6niVhhyaW5GWSPFc=",
-    "wifi" : "VD5LBXBTRH3K37YW"
-    }
